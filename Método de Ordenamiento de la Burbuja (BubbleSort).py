b2e3ed52750fcdee80cc493f50b33ec77520f94d
Jauregui-1 <jaredrj03@gmail.com>
2024-11-17 12:45:38 -0600

    Bubble Sort

---
+++ Método de Ordenamiento de la Burbuja (BubbleSort).py
@@ -0,0 +1,38 @@
+# El Ordenamiento de burbuja (BubbleSort) es un algoritmo de ordenamiento simple. 
+# El mismo funciona revisando cada elemento de la lista a ordenar con el que le sigue, 
+# cambiándolos de posición si están en un orden incorrecto (n>n+1). 
+# Es necesario repetir este proceso varias veces hasta que no se necesiten más cambios, 
+# lo que significa que la lista quedó ordenada.
+
+
+from random import sample 
+# Importamos un Método de la biblioteca random para generar listas aleatorias
+
+lista = list(range(100)) # Creamos la lista base con números del 1 al 100
+
+# Creamos una lista aleatoria con sample 
+#(8 elementos aleatorios de la lista base)
+vectorbs = sample(lista,8) 
+
+
+def bubblesort(vectorbs):
+    """Esta función ordenara el vector que le pases como argumento con el Método de Bubble Sort"""
+    
+    # Imprimimos la lista obtenida al principio (Desordenada)
+    print("El vector a ordenar es:",vectorbs)
+    n = 0 # Establecemos un contador del largo del vector
+    
+    for _ in vectorbs:
+        n += 1 #Contamos la cantidad de caracteres dentro del vector
+    
+    for i in range(n-1): 
+    # Le damos un rango n para que complete el proceso. 
+        for j in range(0, n-i-1): 
+            # Revisa la matriz de 0 hasta n-i-1
+            if vectorbs[j] > vectorbs[j+1] :
+                vectorbs[j], vectorbs[j+1] = vectorbs[j+1], vectorbs[j]
+            # Se intercambian si el elemento encontrado es mayor 
+            # Luego pasa al siguiente
+    print ("El vector ordenado es: ",vectorbs)
+
+bubblesort(vectorbs)
